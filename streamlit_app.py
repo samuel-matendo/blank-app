956fb2ca4e3067d2bea567df4fd18f75c0e65d15
samuel-matendo <matendosamuel25@gmail.com>
2025-03-02 23:15:28 +0000

    Add multi-page structure

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,25 @@
 import streamlit as st
+from pages.page_1 import func_page_1
+from pages.page_2 import func_page_2
 
-st.set_page_config(
-    page_title='Hello world',
-    layout='centered',
-    initial_sidebar_state='auto',
-    menu_items={
-        'Get Help': 'https://streamlit.io/',
-        'Report a bug': 'https://github.com',
-        'About': 'About your application: **Hello world**'
+def main():
+    st.sidebar.subheader('Page selection')
+    page_selection = st.sidebar.selectbox('Please select a page',['Main Page',
+    'Page 1','Page 2'])
+    pages_main = {
+        'Main Page': main_page,
+        'Page 1': run_page_1,
+        'Page 2': run_page_2
     }
-)
 
-st.sidebar.title('Hello world')
-st.title('Hello world')
+    # Run selected page
+    pages_main[page_selection]()
+
+def main_page():
+    st.title('Main Page')
+def run_page_1():
+    func_page_1()
+def run_page_2():
+    func_page_2()
+if __name__ == '__main__':
+    main()
